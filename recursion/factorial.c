629bfd3c0621c3f242ff732cc14cc97a7f0f40ee
sudhi76 <sudhigupta2299@gmail.com>
2020-01-09 01:05:42 +0530

    factorial using  recursion

---
+++ recursion/factorial.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int fact(int n)
+{
+    if (n==0)
+        return 1;
+    else
+        return fact(n-1)*n;
+}
+int main()
+{
+    int r;
+    r=fact(10);
+    printf("%d",r);
+    return 0;
+
+}
