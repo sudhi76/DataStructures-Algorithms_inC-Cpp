55795a7c991e21edd6d2d1dcf9ad788d24d53a6d
sudhi76 <sudhigupta2299@gmail.com>
2020-01-09 15:57:19 +0530

    day 2 of 100daysofcode

---
+++ recursion/tower_of_hanaoi.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void TOH(int n, int a, int b, int c)
+{
+    if(n>0)
+    {
+        TOH(n-1,a,c,b);
+        printf("(%d, %d)\n", a,c);
+        TOH(n-1,a,b,c);
+    }
+}
+int main()
+{
+    TOH(3,1,2,3);
+    return 0;
+}
