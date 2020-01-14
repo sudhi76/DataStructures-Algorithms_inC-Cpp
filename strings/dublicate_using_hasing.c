f47f0156e2c3624d05744d45e22626e808a19ed9
sudhi76 <sudhigupta2299@gmail.com>
2020-01-14 21:38:30 +0530

    day 7 of 100 days of code

---
+++ strings/dublicate_using_hasing.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    char A[]="finding";
+    int H[26],i;
+    for(i=0;A[i]!='\0';i++)
+    {
+        H[A[i]-97]+=1;
+    }
+    for(i=0;i<26;i++)
+    {
+        if(H[i]>1)
+        {
+            printf("%c",i+97);
+        printf("%d",H[i]);
+        }
+    }
+return 0;
+}
